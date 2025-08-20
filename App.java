82f042edede8812612c2d493e8da7f1f8e5890e5
Akshat Garg <165119239+akshatgarg9268@users.noreply.github.com>
2025-08-20 10:41:35 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+package com.verizon.training.dockerspringdemo;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jenkins123!" );
+    }
+}
